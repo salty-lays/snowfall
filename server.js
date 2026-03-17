ec67171c9341df21fde0004da07dcfc67cd93cc3
salty-lays <chetanpatil21444@gmail.com>
2026-03-17 21:15:13 +0530

    added

--- server.js
+++ server.js
@@ -2,16 +2,31 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 const PORT = 3000;
 
-// Serve client page
-app.get('/', (req,res)=>res.sendFile(__dirname+'/index.html'));
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'public', 'register.html')));
+app.get('/dashboard', (req, res) => res.sendFile(path.join(__dirname, 'public', 'dashboard.html')));
 
 // Socket handling
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
   console.log('Client connected');
-  socket.on('host_input', (data)=>io.emit('update_field',data));
-  socket.on('stop_generation', ()=>io.emit('stop_generation'));
+
+  socket.on('host_input', (data) => {
+    io.emit('update_field', data);
+  });
+
+  socket.on('stop_generation', () => {
+    io.emit('stop_generation');
+  });
+
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-http.listen(PORT, ()=>console.log(`Server running at http://localhost:${PORT}`));
+// Start server
+http.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
